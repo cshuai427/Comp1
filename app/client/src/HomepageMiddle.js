e2e6e6685fab4ee6346833ab356fe48ba6b52505
tina789zhao <tina789zhao@gmail.com>
2018-08-12 21:30:49 +1000

    add react of homepage.

---
+++ app/client/src/HomepageMiddle.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class HomepageMiddle extends Component {
+
+    render(){
+        return(
+
+                <div>
+
+                    <h1>event</h1>
+                    <text>post</text>
+
+                </div>
+
+
+
+        );
+    }
+
+}
+export default HomepageMiddle;
---
+++ app/client/src/HomepageRight1.js
@@ -0,0 +1,19 @@
+
+import React, { Component } from 'react';
+
+class HomepageRight1 extends Component {
+
+    render(){
+        return(
+
+            <div>
+              <h1>Friden</h1>
+            </div>
+
+
+
+        );
+    }
+
+}
+export default HomepageRight1;

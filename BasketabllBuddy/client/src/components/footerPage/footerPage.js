b0696ff4a28eb635aba327898d7ffe8ab3f610f3
tiansang <12652350@student.uts.edu.au>
2018-08-19 21:46:19 +1000

    need to be fix

--- BasketabllBuddy/client/src/components/footerPage/footerPage.js
+++ BasketabllBuddy/client/src/components/footerPage/footerPage.js
@@ -1,8 +1,13 @@
 import React, {Component} from 'react';
+import Logo from '../../Img/logo.jpg';
 
 class FooterPage extends Component {
     render(){
         return(
+                /* shuai, fix this css
+                <td>
+            <img src={Logo}/>
+                */
           <h1>Cpoyright © 2018 BasketballBuddy</h1>
         );
 

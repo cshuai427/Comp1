b4588a95a83abf395dec726dcbcafa618243da4e
tiansang <12652350@student.uts.edu.au>
2018-08-19 21:04:07 +1000

    update components to App

--- BasketabllBuddy/client/src/App.js
+++ BasketabllBuddy/client/src/App.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import HeaderPage from "./components/headerPage/headerPage";
+import HomePageLeft from "./components/accountPage/homeAccountPage";
+import HomeEventPage from "./components/eventPage/homeEventPage";
+import HomePageRight from "./components/contactsPage/homeFriendList";
+import FooterPage from "./components/footerPage/footerPage";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+        <div>
+            <div className="App-header">    <HeaderPage /></div>
+            <div className="App-left"> <HomePageLeft /> </div>
+            <div className="App-middle"> <HomeEventPage/> </div>
+            <div className="App-right"> <HomePageRight /> </div>
+            <div className="App-footer"> <FooterPage /> </div>
+
+        </div>
+
+    );
+
+
+
+
+
+      /*<div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
@@ -14,7 +33,8 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
+    */
+
   }
 }
 

3c8a3d08379d650b6be9cfb8774ff1b6aecde645
tiansang <12652350@student.uts.edu.au>
2018-08-19 22:22:28 +1000

    finish eventpage exampple

--- BasketabllBuddy/client/src/components/eventPage/homeEventPage.js
+++ BasketabllBuddy/client/src/components/eventPage/homeEventPage.js
@@ -7,8 +7,22 @@ class HomeEventPage extends Component {
 
                 <div>
 
-                    <h1>event</h1>
-                    <text>post</text>
+                    <h1>Basketball Meetup Events</h1>
+                    <div>
+                    <form method="get">
+                        <img src="/" width="50%" height="20%"  />
+                        <h3>Saturday Event</h3>
+                        <p>Hello everyone, this Saturday I will play basketball at central. Anyone has time could join me. Welcome <a href="/"> View</a></p>
+                        <hr/>
+                        <h3>Sunday Event</h3>
+                        <p>Hello everyone, this Saturday I will play basketball at central. Anyone has time could join me. Welcome<a href="/"> View</a></p>
+                        <hr/>
+                        <img src="/" width="50%" height="20%"  />
+                        <h3>Saturday Event</h3>
+                        <p>Hello everyone, this Saturday I will play basketball at central. Anyone has time could join me. Welcome <a href="/"> View</a></p>
+                        <hr/>
+                    </form>
+                    </div>
 
                 </div>
 

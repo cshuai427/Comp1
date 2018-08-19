1f8c5f7144d24990075e715fb4eac0072d4e421d
tiansang <12652350@student.uts.edu.au>
2018-08-19 21:04:33 +1000

    rewrite index structure

--- BasketabllBuddy/client/src/index.js
+++ BasketabllBuddy/client/src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from 'App';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import HomepageMiddle from "./components/eventPage/HomepageMiddle";
-import HomepageLeft from "./components/accountPage/HomepageLeft";
-import HomepageRight1 from "./components/contactsPage/HomepageRight1"
 
 
-ReactDOM.render(<HomepageLeft />, document.getElementById('left'));
-ReactDOM.render(<HomepageMiddle />, document.getElementById('middle'));
-ReactDOM.render(<HomepageRight1 />, document.getElementById('right'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 
 registerServiceWorker();

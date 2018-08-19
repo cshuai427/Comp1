54fdf4d70d2884eecf48b3df644fdb4d8770d24d
tiansang <12652350@student.uts.edu.au>
2018-08-19 21:02:51 +1000

    Add components include header,leftpage, rightpage and middlepage.

---
+++ BasketabllBuddy/client/src/components/footerPage/footerPage.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class FooterPage extends Component {
+    render(){
+        return(
+          <h1>Cpoyright © 2018 BasketballBuddy</h1>
+        );
+
+
+    }
+}
+
+export default FooterPage;

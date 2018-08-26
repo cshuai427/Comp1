c81e7ad10d58a951e85dfe1f80ed1f3e5d9b7578
tiansang <12652350@student.uts.edu.au>
2018-08-27 03:07:09 +1000

    Add post validation

---
+++ BasketabllBuddy/server/validation/post.js
@@ -0,0 +1,47 @@
+const validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validationPost(data) {
+    let errors = {};
+
+    data.eventText = !isEmpty(data.eventText) ? data.eventText : '';
+    data.eventTitle = !isEmpty(data.eventTitle) ? data.eventTitle : '';
+    data.eventPeopleNumber = !isEmpty(data.eventPeopleNumber) ? data.eventPeopleNumber : '';
+    data.eventLocation = !isEmpty(data.eventLocation) ? data.eventLocation : '';
+    data.haveBall = !isEmpty(data.haveBall) ? data.haveBall : '';
+    data.eventDate = !isEmpty(data.eventDate) ? data.eventDate : '';
+
+    if(!validator.isLength(data.eventText, {min: 15, max: 300})){
+        errors.eventText = 'Text must be at least 15 and 300 characters';
+    }
+
+    if(validator.isEmpty(data.eventText)){
+        errors.eventText = 'Text field is required';
+    }
+
+    if(validator.isEmpty(data.eventTitle)){
+        errors.eventTitle = 'Title field is required';
+    }
+
+    if(validator.isEmpty(data.eventPeopleNumber)){
+        errors.eventPeopleNumber = 'People number field is required';
+    }
+
+    if(validator.isEmpty(data.eventLocation)){
+        errors.eventLocation = 'Location field is required';
+    }
+
+    if(validator.isEmpty(data.haveBall)){
+        errors.haveBall = 'Ball field is required';
+    }
+
+    if(validator.isEmpty(data.eventDate)){
+        errors.eventDate = 'Event start date field is required';
+    }
+
+
+    return{
+        errors,
+        isValid: isEmpty(errors)
+    };
+};

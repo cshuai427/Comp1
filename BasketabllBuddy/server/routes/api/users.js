33b53299bdbe77c25de2f2a5024ad6f9ffd4de7c
tiansang <12652350@student.uts.edu.au>
2018-08-27 00:18:19 +1000

    Fix token configuration

--- BasketabllBuddy/server/routes/api/users.js
+++ BasketabllBuddy/server/routes/api/users.js
@@ -111,7 +111,7 @@ router.post('/login', (req, res) => {
                             (err, token) => {
                                 res.json({
                                     success: true,
-                                    token: 'BasketballBuddy' + token
+                                    token: 'Bearer ' + token
                                 });
                             });
                     } else {

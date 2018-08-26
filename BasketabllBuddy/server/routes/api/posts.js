91385fd460cadf466f521207a3964e6bbe9176ca
tiansang <12652350@student.uts.edu.au>
2018-08-27 03:22:29 +1000

    Add post api for searching by post id

--- BasketabllBuddy/server/routes/api/posts.js
+++ BasketabllBuddy/server/routes/api/posts.js
@@ -79,4 +79,33 @@ router.post('/', passport.authenticate('jwt', { session: false}),
     });
 
 
+//  @route  Delete api/posts/:id
+//  @desc   Delete post
+//  @access Private
+
+router.delete('/:id', passport.authenticate('jwt', { session: false}),
+    (req, res)=>
+    {
+        Profile.findOne({ user: req.user.id })
+            .then(profile => {
+                Post.findById(req.params.id)
+                    .then(post => {
+                        // Check for post owner
+                        if(post.user.toString() !== req.user.id){
+                            return res.status(401).json({ notauthorized: 'User not authorized'});
+
+                        }
+
+                        // Delete
+
+                        post.remove().then(() => res.json({ success: true}));
+                    })
+                    .catch(err => res.status(404).json({ postnotfound: 'Not post found'}));
+            });
+    });
+
+
+
+
+
 module.exports = router;

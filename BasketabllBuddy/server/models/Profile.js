589e50ead63ff934f1883ec476428a5ee961d485
tiansang <12652350@student.uts.edu.au>
2018-08-26 21:13:46 +1000

    Create database Schema --Profile

---
+++ BasketabllBuddy/server/models/Profile.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const ProfileSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    name: {
+        type: String,
+        required: true,
+        max: 40
+    },
+    interests: {
+        type: [String]
+    },
+    playerRole: {
+        type: String
+    },
+    aboutMe: {
+        type: String
+
+    },
+    social:
+        {
+            youtube: {
+                type: String
+            },
+            twitter: {
+                type: String
+            },
+            facebook: {
+                type: String
+            },
+            linkedin: {
+                type: String
+            },
+            instargram: {
+                type: String
+            }
+
+        },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);

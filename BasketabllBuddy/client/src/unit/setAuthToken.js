d64cad62d96817e46e7283e6bfa0eec9089b99ef
tina789zhao <tina789zhao@gmail.com>
2018-09-02 17:30:37 +1000

    Two Action Type:GET_ERRORS and SET_CURRENT_USER.

---
+++ BasketabllBuddy/client/src/unit/setAuthToken.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+const setAuthToken = token =>{
+    if (token){
+        axios.defaults.headers.common['Authorizaion']=token;
+
+    }else{
+        delete axios.defaults.headers.common['Authorizaion'];
+
+
+    }
+};
+export default setAuthToken;

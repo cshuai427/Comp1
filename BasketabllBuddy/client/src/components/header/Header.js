5fc77e02459d88ef4f43beb00c0a48b9e486245b
tina789zhao <tina789zhao@gmail.com>
2018-09-02 17:08:46 +1000

    complete the header control login and logout.

--- BasketabllBuddy/client/src/components/header/Header.js
+++ BasketabllBuddy/client/src/components/header/Header.js
@@ -1,19 +1,36 @@
 import React, {Component} from 'react';
 import logo from "../../Img/logo.png";
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {logoutUser} from "../../actions/authActions";
+import PropTypes from 'prop-types';
+
 
 class Header extends Component{
+    onLogoutclick(e){
+        e.preventDefault();
+
+        this.props.logoutUser();
+    }
     render()
     {
+        const {isAuthenticated, user} =this.props.auth;
+        const authLinks = ( <ul><li><a href="#" onClick={this.onLogoutclick.bind(this)}>
+            <img className="rounded-circle" src={user.avatar} style={{width:'25px',marginRight:'5px'}} alt={user.name} title="email"/>{' '}Logout</a></li>
+                   </ul>);
+        const guestLinks = ( <ul>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/register">Register</Link></li></ul>);
         return(
             <header>
         <ul>
         <li><img src = { logo } alt="logo" /></li>
         <li><a className="active" href="/">Home</a></li>
-        <li><a href="/event/post">Event</a></li>
+        <li><Link to="/event/post">Event</Link></li>
         <li><a href="">Account</a></li>
-            <li><a href="/login">login</a></li>
-            <li><a href="/register">Register</a></li>
+
+            {isAuthenticated ? authLinks : guestLinks}
+
         </ul>
             </header>
         )
@@ -22,5 +39,12 @@ class Header extends Component{
 
 
 }
-
-export default Header;
+Header.protoTypes={
+    logoutUser:PropTypes.func.isRequired,
+    auth:PropTypes.object.isRequired
+}
+const mapStateToProps=state=>({
+    auth:state.auth,
+    errors: state.errors
+});
+export default connect(mapStateToProps,{logoutUser})(Header);

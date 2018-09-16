3aeb331b4d53363fbe3066aef92a9d759caa06a1
catherine <catherinechen427@gmail.com>
2018-09-17 02:26:46 +1000

    Added the post reducer

--- BasketabllBuddy/client/src/reducers/index.js
+++ BasketabllBuddy/client/src/reducers/index.js
@@ -1,7 +1,9 @@
 import {combineReducers} from 'redux';
 import authReducer from './authReducer';
 import errorReduce from './errorReducer';
+import postReducer from './postReducer';
 export default combineReducers({
     auth: authReducer,
-    errors:errorReduce
+    errors:errorReduce,
+    post:postReducer
 });

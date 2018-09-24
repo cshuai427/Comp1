881d49bee27d73e1182b9c01e5c9686cb46735f2
catherine <catherinechen427@gmail.com>
2018-09-24 10:08:28 +1000

    Added PostFeed file using for posts

---
+++ BasketabllBuddy/client/src/components/posts/PostFeed.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import PostItem from './PostItem';
+
+class PostFeed extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+
+}
+
+export default PostFeed;

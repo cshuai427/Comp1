fa943b68e8282b514bda4219c7eb9da1f721ae48
tiansang <12652350@student.uts.edu.au>
2018-09-30 19:11:28 +1000

    test

--- BasketabllBuddy/client/src/reducers/index.js
+++ BasketabllBuddy/client/src/reducers/index.js
@@ -1,9 +1,7 @@
 import {combineReducers} from 'redux';
 import authReducer from './authReducer';
 import errorReduce from './errorReducer';
-import postReducer from './postReducer';
 export default combineReducers({
     auth: authReducer,
-    errors:errorReduce,
-    post:postReducer
+    errors:errorReduce
 });

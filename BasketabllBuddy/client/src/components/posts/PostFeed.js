e6ac405b45fe9c55e1b5b5cdb9f3deec0e92949c
catherine <catherinechen427@gmail.com>
2018-09-30 17:29:06 +1000

    Modified postFeed

--- BasketabllBuddy/client/src/components/posts/PostFeed.js
+++ BasketabllBuddy/client/src/components/posts/PostFeed.js
@@ -4,13 +4,12 @@ import PostItem from './PostItem';
 
 class PostFeed extends Component {
     render() {
-        return (
-            <div>
-
-            </div>
-        )
+        const { posts } = this.props;
+        return posts.map(post => <PostItem key ={ post._id} post={post} />)
     }
 
 }
-
+PostFeed.propTypes = {
+    posts: PropTypes.array.isRequired
+}
 export default PostFeed;

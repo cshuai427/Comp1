cd584370268b80258e460f36aa8523917163f13d
tiansang <12652350@student.uts.edu.au>
2018-10-03 06:07:47 +1000

    Fix bug: wrong typo=> instagram

--- BasketabllBuddy/server/models/Profile.js
+++ BasketabllBuddy/server/models/Profile.js
@@ -35,7 +35,7 @@ const ProfileSchema = new Schema({
             linkedin: {
                 type: String
             },
-            instargram: {
+            instagram: {
                 type: String
             }
 

43f31297d370024ae21e824b45a4f5738c745664
tiansang <12652350@student.uts.edu.au>
2018-10-03 17:21:58 +1000

    Fix css for Account and FirendList component

--- BasketabllBuddy/client/src/components/account/Account.js
+++ BasketabllBuddy/client/src/components/account/Account.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { getCurrentProfile } from '../../actions/profileActions';
 
 class Account extends Component {
 
+    componentDidMount(){
+        this.props.getCurrentProfile();
+    }
+
     render(){
+
+        const { user } = this.props.auth;
+
+        const { profile } = this.props.profile;
         return(
-            <div className="col-sm-2 col-auto sidenav">
+            <div className="col-sm-2 col-auto sidenav bg-secondary">
                     <h3>My account</h3>
-                    <img width="100" height="200" alt="avatar"/>
-                    <h3>Username123</h3>
-                    <h3>Post 20</h3>
-                    <h3>Friend 10</h3>
-                    <a  href="/">Edit</a>
+                    <img className="rounded-circle"
+                         src={user.avatar}
+                         style={{width:'100px',marginRight:'5px'}}
+                         alt={user.name}
+                         title={user.name}
+                    />
+
+                    <h3>{user.name}</h3>
+                    <h3>Post: {}</h3>
+                    <h3>Friend: {user.friendList === null ? Object.keys(user.friendList).length : '0'}</h3>
+                    <a  href="/profile-view">Edit</a>
             </div>
 
 
@@ -19,4 +36,17 @@ class Account extends Component {
     }
 
 }
-export default Account;
+
+Account.propTypes = {
+    getCurrentProfile: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    profile: PropTypes.object.isRequired
+
+};
+
+const mapStateToProps = state =>({
+    auth: state.auth,
+    profile: state.profile
+});
+
+export default connect(mapStateToProps, { getCurrentProfile })(Account);
--- BasketabllBuddy/client/src/components/contacts/FriendList.js
+++ BasketabllBuddy/client/src/components/contacts/FriendList.js
@@ -5,7 +5,7 @@ class FriendList extends Component {
 
     render(){
         return(
-            <div className="col-sm-2 col-auto sidenav">
+            <div className="col-sm-2 col-auto sidenav bg-secondary">
                  <h2>Online Chat</h2>
                 <p><img src="/" alt="avatar"/> Tom 123</p>
                 <p><img src="/" alt="avatar"/> Peter 456</p>

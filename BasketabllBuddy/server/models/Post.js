c2316a7772df4bcc760b82d59ac212f72e8478d7
catherine <catherinechen427@gmail.com>
2018-10-05 16:27:15 +1000

    Updated data scheme for the attending people numbers.

--- BasketabllBuddy/server/models/Post.js
+++ BasketabllBuddy/server/models/Post.js
@@ -19,6 +19,12 @@ const PostSchema = new Schema({
         type: Number,
         required: true
     },
+    eventAttendPeople: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'users'
+        }
+    ],
     eventLocation: {
         type: String,
         required: true

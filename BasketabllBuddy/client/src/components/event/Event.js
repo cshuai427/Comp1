b3a60848d3f6fb4ab70b687f0abd200ede990f38
catherine <catherinechen427@gmail.com>
2018-10-06 16:17:59 +1000

    Updated the event file to show the events page.

--- BasketabllBuddy/client/src/components/event/Event.js
+++ BasketabllBuddy/client/src/components/event/Event.js
@@ -1,33 +1,60 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import isEmpty from '../../validation/is-empty';
+
+// Should be removed
 import basketball1 from "../../Img/basketball1.jpeg";
 import basketball2 from "../../Img/basketball2.jpg";
 
+// components
+import { getPosts } from '../../actions/postActions';
+import Spinner from '../common/Spinner';
+import EventFeed from './EventFeed';
+
 class Event extends Component {
 
+
+    componentDidMount(){
+        this.props.getPosts();
+    }
+
     render(){
+
+        const { posts, loading } = this.props.post;
+        let postContent;
+
+        if(isEmpty(posts) || loading){
+            postContent = <Spinner/>
+        }
+        else
+        {
+            postContent = <EventFeed posts={posts}/>
+        }
+
         return(
 
-                <div className="Event">
+            <div className="Event">
 
-                    <h1>Basketball Meetup Events</h1>
-                    <hr />
-                    <div>
-                    <form method="get">
-                        <img src={ basketball1 } width="200" height="100"  />
-                        <h3>Saturday Event</h3>
-                        <p>Hello everyone, this Saturday I will play basketball at central. Anyone has time could join me. Welcome <a href="/event/commit"> View</a></p>
-                        <hr/>
-                        <h3>Sunday Event</h3>
-                        <p>Hello everyone, this Saturday I will play basketball at central. Anyone has time could join me. Welcome<a href="/event/commit"> View</a></p>
-                        <hr/>
-                        <img src={ basketball2 } width="200" height="100"  />
-                        <h3>Saturday Event</h3>
-                        <p>Hello everyone, this Saturday I will play basketball at central. Anyone has time could join me. Welcome <a href="/event/commit"> View</a></p>
-                        <hr/>
-                    </form>
-                    </div>
-
+                <h1>Basketball Meetup Events</h1>
+                <hr />
+                <div>
+                    < img src={ basketball1 } width="200" height="100"  />
+                    <h3>Saturday Event</h3>
+                    <p>Hello everyone, this Saturday I will play basketball at central. Anyone has time could join me. Welcome <a href=" "> View</a ></p >
+                    <hr/>
+                    <h3>Sunday Event</h3>
+                    <p>Hello everyone, this Saturday I will play basketball at central. Anyone has time could join me. Welcome<a href="/event/commit"> View</a ></p >
+                    <hr/>
+                    < img src={ basketball2 } width="200" height="100"  />
+                    <h3>Saturday Event</h3>
+                    <p>Hello everyone, this Saturday I will play basketball at central. Anyone has time could join me. Welcome <a href="/event/commit"> View</a ></p >
+                    <hr/>
+                    {postContent}
                 </div>
+
+            </div>
 
 
 
@@ -35,4 +62,16 @@ class Event extends Component {
     }
 
 }
-export default Event;
+
+
+Event.propTypes = {
+    getPosts: PropTypes.func.isRequired,
+    post: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state =>({
+    post: state.post
+});
+
+
+export default connect(mapStateToProps, {getPosts})(Event);

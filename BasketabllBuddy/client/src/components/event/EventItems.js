83a7b1610dc2bec763ff755f4f7f7b250aa487e3
tiansang <12652350@student.uts.edu.au>
2018-10-08 01:58:18 +1100

    Update

--- BasketabllBuddy/client/src/components/event/EventItems.js
+++ BasketabllBuddy/client/src/components/event/EventItems.js
@@ -15,9 +15,9 @@ class EventItems extends Component {
                 marginTop: '3%',
                 marginBottom: '3%',
                 borderRadius: '10px',
-                paddingTop: '15px', paddingBottom:'15px', height:'300p',overflow:'auto'}}>
+                paddingTop: '15px', height:'240px'}}>
                 <div className="events1 col-xl-3 col-sm-3 col-lg-3 col-xs-3">
-                    <div className="imageItem img-responsive" style={{align:'center',textAlign:'left'}}>
+                    <div className="imageItem img-responsive" style={{align:'left'}}>
                         < img src={ basketball1 } style={{width:'100%',height:'100%',position:'relative',}}  alt={post.photo} />
                     </div>
                     {/*<div className="">*/}
@@ -39,7 +39,7 @@ class EventItems extends Component {
                         {post.eventDate}
                         </p>
                         <p>
-                            Number of attend: {post.eventAttendPeople.length} of {post.eventPeopleNumber}
+                        {post.eventAttendPeople}
                         </p>
                             <Link to={`/post/${post._id}`} className="badge badge-primary badge-pill">
                                 View

9c7bc1cbf9f441d9c5f3f997adb1e5b80423a399
tina789zhao <tina789zhao@gmail.com>
2018-10-08 05:02:30 +1100

    fix bug and change css

--- BasketabllBuddy/client/src/components/post/CommentItem.js
+++ BasketabllBuddy/client/src/components/post/CommentItem.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { deleteComment } from '../../actions/postActions';
+import { Link } from 'react-router-dom';
 
 class CommentItem extends Component {
 
@@ -14,21 +15,29 @@ class CommentItem extends Component {
         const { comment, postId, auth } = this.props;
 
         return (
-            <div className="card card-body mb-3">
-                <div className="row">
-                    <div className="col-md-2">
-                        <a href={`/profile/nickname/${comment.nickName}`} >
-                            < img className="rounded-circle d-none d-md-block"
-                                  src={comment.avatar} alt=""/>
-                        </a >
-                        <br/>
-                        <p className="text-center">{comment.nickName}</p >
+            <div className="Comment">
+
+                <div className="container m-3 py-3 pl-0 row border rounded shadow-sm bg-light">
+                    <div className="col-2">
+                        <Link to={`/profile/nickname${comment.nickName}`}>
+                            <img
+                                src={comment.avatar}
+                                className="rounded-circle w-100 shadow"
+                                alt={comment.nickName}
+                            />
+                        </Link>
+
+                        <p className="font-weight-bold py-2 mb-0">{comment.nickName}</p >
                     </div>
-                    <div className="col-md-10">
-                        <p className="lead">{comment.text}</p >
+                    <div className="col-9 rounded-left bg-light border border-right-0 mt-0 text-left">
+                        <h5 className="d-inline-block font-italic py-3 px-1">
+                            {comment.text}
+                        </h5>
+                    </div>
+                    <div className="col-1 px-1">
                         {comment.user === auth.user.id ?
                             (<button onClick={this.onDeleteClick.bind(this, postId, comment._id)}
-                                     type="button" className="btn btn-danger mr-1">
+                                     type="button" className="btn btn-sm btn-outline-danger h-100 mb-2 w-100">
                                 <i className="fas fa-times" />
                             </button>):null}
                     </div>
